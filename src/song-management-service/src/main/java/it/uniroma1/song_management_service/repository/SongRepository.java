71a3b9153d629b5030b7d52c856d6698fc4450c8
Alessandro Rocchi <102317395+TheXbomber@users.noreply.github.com>
2025-10-04 18:12:37 +0200

    First version of song management service

---
+++ src/song-management-service/src/main/java/it/uniroma1/song_management_service/repository/SongRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma1.song_management_service.repository;
+
+import it.uniroma1.song_management_service.model.Song;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface SongRepository extends MongoRepository<Song, String> {
+    List<Song> findByArtist(String artist);
+}
---
+++ src/song-management-service/src/test/java/it/uniroma1/song_management_service/SongManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma1.song_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
